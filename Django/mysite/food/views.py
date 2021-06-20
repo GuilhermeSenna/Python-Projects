c55513191a1719fdd504a1f140fb5f470fe1ba8b
zBiggui <bigguisenna@gmail.com>
2021-06-19 22:14:08 -0300

    Django project from Udemy Course

---
+++ Django/mysite/food/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse('Hello World')
+
+
+def item(request):
+    return HttpResponse('This is an item view')
