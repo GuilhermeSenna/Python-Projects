fbfda8b97e5a9b6d49e20ce6002755e0846a652b
zBiggui <bigguisenna@gmail.com>
2021-06-19 23:08:03 -0300

    Templates adicionados

--- Django/mysite/food/views.py
+++ Django/mysite/food/views.py
@@ -1,12 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Item
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello World')
+    item_list = Item.objects.all()
+    context = {
+        'item_list': item_list,
+    }
+
+    return render(request, 'food/index.html', context)
 
 
 def item(request):
     return HttpResponse('This is an item view')
+
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item':item,
+    }
+
+    return render(request, 'food/detail.html', context)
