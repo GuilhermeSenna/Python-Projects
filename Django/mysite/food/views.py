1c30d8ac055a34ac3e1aac5d7fb50d62f13cc14b
zBiggui <bigguisenna@gmail.com>
2021-06-26 23:40:36 -0300

    Signals + Class Based Views + Redesign
    - Uso de Signals
    > Criando um perfil de usuário automaticamente após um usuário se registrar
    - Classe Based Views
    > Alguns views antes feitos usando funções padrões de Python foram refeitar com Classe Based Views
    - Adicionando criador do post automaticamente
    -  Redesign de algumas páginas utilizando bootstrap

--- Django/mysite/food/views.py
+++ Django/mysite/food/views.py
@@ -2,10 +2,12 @@ from django.http import HttpResponse
 from .forms import ItemForm
 from .models import Item
 from django.shortcuts import render, redirect
-
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 # Create your views here.
 
-
+'''
 def index(request):
     item_list = Item.objects.all()
     context = {
@@ -13,12 +15,20 @@ def index(request):
     }
 
     return render(request, 'food/index.html', context)
+'''
+
+
+# Substitui o index original utilizando class based view
+class IndexClassView(ListView):
+    model = Item
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'
 
 
 def item(request):
     return HttpResponse('This is an item view')
 
-
+'''
 def detail(request, item_id):
     item = Item.objects.get(pk=item_id)
     context = {
@@ -26,6 +36,13 @@ def detail(request, item_id):
     }
 
     return render(request, 'food/detail.html', context)
+'''
+
+
+# Substitui o detail original utilizando class based view
+class FoodDetail(DetailView):
+    model = Item
+    template_name = 'food/detail.html'
 
 
 def create_item(request):
@@ -36,6 +53,19 @@ def create_item(request):
         return redirect('food:index')
 
     return render(request, 'food/item-form.html', {'form': form})
+
+
+# this is a class based item for the create_item
+
+class CreateItem(CreateView):
+    model = Item
+    fields = ['item_name', 'item_desc', 'item_price', 'item_image']
+    template_name = 'food/item-form.html'
+
+    def form_valid(self, form):
+        form.instance.user_name = self.request.user
+
+        return super().form_valid(form)
 
 
 def update_item(request, id):
